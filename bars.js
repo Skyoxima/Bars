49c3eacd59a6c259d14ee148ceaba20c880f3be5
SamarthTumdi <samarth.tumdi@gmail.com>
2023-02-09 22:10:13 +0530

    Adding Bars semi-complete

--- bars.js
+++ bars.js
@@ -11,32 +11,54 @@
   }
 })();
 
-(function yAxisBarNums() {
+function yAxisBarNums() {
   barDivs = document.querySelectorAll('.bar');
   for(let i = 0; i < barDivs.length; i++)
     barDivs[i].style.setProperty("--bar-number", `"${i + 1}"`);
-})();
+};
+yAxisBarNums();
 
 (function addBar() {
-  addBarBtn = document.getElementById('addBarBtn');
+  xPlaneDiv = document.querySelector('.x-plane');
+  addBarBtn = document.getElementById('add-bar-btn');
   blockerDiv = document.querySelector('.blocker');
   popUpDiv = document.querySelector('.pop-up');
-  newBarTxt = document.getElementById("new-bar-val");
+  newBarVal = document.getElementById("new-bar-val");
+  newBarLabel = document.getElementById("new-bar-label");
   popUpSubmitBtn = document.getElementById("pop-up-submit");
-
+  popUpCancelBtn = document.getElementById("pop-up-cancel");
+  
   addBarBtn.onclick = () => {
+    // newBarVal.value = "";
+    popUpSubmitBtn.disabled = true;
     blockerDiv.style.display = 'block';
   }
   
-  newBarTxt.oninput = () => {
-    newBarTxt.value = newBarTxt.value.replace(/[^0-9]/g, "");
-    // add positive lookahead for next digits, (only 0 allowed after 1 and nothing allowed after 2-9)
-    if(parseInt(newBarTxt.value) > 0) {
-      popUpSubmitBtn.disabled = false;
+  // acutal addition mechanism
+  newBarVal.oninput = () => {
+    newBarVal.value = newBarVal.value.replace(/[^0-9]/g, "");     // don't allow any text other than digits
+    if(newBarVal.value === "")                                    // disable the add button for all invalid inputs i.e no value to set to
+      popUpSubmitBtn.disabled = true;
+    else {
+      // can add warning on label PsE for invalid values
+      newBarVal.value = newBarVal.value.replace(/1[1-9]+|[2-9][0-9]+|10[\d]+/g, newBarVal.value[0]);
+      if(parseInt(newBarVal.value) > 0)
+        popUpSubmitBtn.disabled = false;
     }
   } 
 
   popUpSubmitBtn.onclick = () => {
+    bar = document.createElement('div');
+    bar.className = 'bar';
+    bar.style.background = ""
+    xPlaneDiv.appendChild(bar);
+    yAxisBarNums();
+    
+    newBarVal.value = "";
+    blockerDiv.style.display = 'none';
+  }
+
+  popUpCancelBtn.onclick = () => {
     blockerDiv.style.display = 'none';
   }
 })();
