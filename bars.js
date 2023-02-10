4f22100ed2859184bb957eb208409f19ad67dcc3
SamarthTumdi <samarth.tumdi@gmail.com>
2023-02-10 17:57:44 +0530

    Basic bars adding done

--- bars.js
+++ bars.js
@@ -1,8 +1,8 @@
-(function xAxisTicks() {
+(function CreateXAxisTicks() {
   xTicks = document.querySelectorAll(".x-ticks");
 
   for(let i = 0; i < xTicks.length; i++) {
-    xTicks[i].style.left = `${20 + 105 * i}px`;
+    xTicks[i].style.left = `${1.75 + 9.6 * i}%`;
     xTicks[i].style.setProperty("--tick-text", `'${i}'`);
     if(i > 6)
       xTicks[i].style.setProperty("--tick-text-color", "green");
@@ -19,42 +19,49 @@ function yAxisBarNums() {
 yAxisBarNums();
 
 (function addBar() {
+  xTicksSpans = document.querySelectorAll('.x-ticks');
   xPlaneDiv = document.querySelector('.x-plane');
   addBarBtn = document.getElementById('add-bar-btn');
   blockerDiv = document.querySelector('.blocker');
   popUpDiv = document.querySelector('.pop-up');
   newBarVal = document.getElementById("new-bar-val");
   newBarLabel = document.getElementById("new-bar-label");
+  newBarColor = document.getElementById("bar-color-picker");
   popUpSubmitBtn = document.getElementById("pop-up-submit");
   popUpCancelBtn = document.getElementById("pop-up-cancel");
   
   addBarBtn.onclick = () => {
-    // newBarVal.value = "";
     popUpSubmitBtn.disabled = true;
     blockerDiv.style.display = 'block';
   }
   
-  // acutal addition mechanism
+  // validation of inputs
   newBarVal.oninput = () => {
     newBarVal.value = newBarVal.value.replace(/[^0-9]/g, "");     // don't allow any text other than digits
     if(newBarVal.value === "")                                    // disable the add button for all invalid inputs i.e no value to set to
       popUpSubmitBtn.disabled = true;
     else {
-      // can add warning on label PsE for invalid values
       newBarVal.value = newBarVal.value.replace(/1[1-9]+|[2-9][0-9]+|10[\d]+/g, newBarVal.value[0]);
-      if(parseInt(newBarVal.value) > 0)
-        popUpSubmitBtn.disabled = false;
+      popUpSubmitBtn.disabled = false;
     }
   } 
 
+  // using the field data to construct the bar
   popUpSubmitBtn.onclick = () => {
     bar = document.createElement('div');
     bar.className = 'bar';
-    bar.style.background = ""
+    bar.style.backgroundImage = "linear-gradient(to right, " + newBarColor.value + " 96%, transparent)";  // do NOT supply the ; in CSS
+    bar.style.setProperty("--stands-for-text", `"${newBarLabel.value}"`);
+    let newBarNumVal = parseInt(newBarVal.value);
+    let iXTickPsn = xTicksSpans[newBarNumVal].getBoundingClientRect();
+    let zXTickPsn = xTicksSpans[0].getBoundingClientRect();          // z ~ zero-th
+    bar.style.width = `${iXTickPsn.left - zXTickPsn.left}px`
     xPlaneDiv.appendChild(bar);
     yAxisBarNums();
     
     newBarVal.value = "";
+    newBarLabel.value = "";
+    newBarColor.value = "#ffc0cb";
     blockerDiv.style.display = 'none';
   }
 
